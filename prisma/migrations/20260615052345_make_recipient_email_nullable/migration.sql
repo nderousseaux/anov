90ae1659503b1d7f336753b8a7fd23e7ec2a4464
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2026-07-02 11:20:06 +0200

    mig

--- prisma/migrations/20260615052345_make_recipient_email_nullable/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `durationMonths` on the `GiftCard` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "GiftCard" DROP COLUMN "durationMonths",
-ALTER COLUMN "recipientEmail" DROP NOT NULL;
